780eb061bf98a9519ef1f7260d2f1470fa161906
Kaeljane <kayyt0102@gmail.com>
2025-09-10 21:28:26 -0300

    .

---
+++ CodeForces/Contest-2/B_Drawing_X.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll unsigned long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin>>n;
+    for(int i = 0 ; i<n ; i++){
+        
+    }
+
+    return 0;
+}
--- CodeForces/ICPC-Sheet #2 (Loops)/Z_Three_Numbers.cpp
+++ CodeForces/ICPC-Sheet #2 (Loops)/Z_Three_Numbers.cpp
@@ -8,52 +8,18 @@ int main() {
     // k s
     ll k, s, qt = 0;
     cin>>k>>s;
-    // determine a quantidade de valores diferentes de x e 
-    if (k<s){
-        for (ll x = 0 ; x <= k ; x++) {
-            for (ll y = 0 ; y <= k ; y++){
-                for (ll z = 0 ; z <= k ; z++) {
-                    if ( (x + y + z) == s) {qt++;}
-                }
-            }
-        }
-    }
-    else {
-        for (ll x = 0 ; x <= s ; x++) {
-            for (ll y = 0 ; y <= s ; y++){
-                for (ll z = 0 ; z <= s ; z++) {
-                    if ( (x + y + z) == s) {qt++;}
-                }
+
+
+    for (ll x = 0 ; x <= k ; x++) {
+        for (ll y = 0 ; y <= k; y++) {
+            ll z = s - x - y;
+            if (0 <= z && z <= k) {
+                qt++;
             }
         }
     }
     cout<< qt << endl;
-    
-    
-    
-    
-    
-    
-    
-    // for (int i = 0 ; i <= 2*k ; i++){
-    //     for (int j = 0 ; j <= k ; i++) {
-    //         if ((i + j) == s) {
-    //             qt++;
-    //         }
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
     return 0;
 }
