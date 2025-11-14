2209b4148ef92fa418d5f434e5577ed9585143bc
Kaeljane <kayyt0102@gmail.com>
2025-11-14 11:19:50 -0300

    Add A_Lucky_Division.cpp | CODEFORCES

---
+++ CodeForces/1000/A_Lucky_Division.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) v.begin(), v.end()
+#define imp(v, t) copy(v.begin(), v.end(), ostream_iterator<t> (cout, ))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 14/11/2025 by Kaeljane
+using namespace std;
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+        12
+        n (1 a 1000)
+*/
+void solve() {
+        cin>>n;
+        bool r = false;
+        vl list = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+
+        for (ll x : list) {
+                if ( n % x == 0) {
+                        r = true;
+                        break;
+                }
+        }
+
+        if (r) cout << "YES" << '\n';
+        else cout << "NO" << '\n';
+}
+signed main() {
+        ___
+        t=1;
+        while(t--)solve();
+        
+        return 0;
+}
