79130f72d0891ef8157b50939c19488eec1cccc4
Kaeljane <kayyt0102@gmail.com>
2025-11-22 18:23:41 -0300

    Add Número Perfeito | Beecrowd

---
+++ Beecrowd/numeroPerfeito.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 22/11/2025 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+        
+        
+*/
+vector<ll> divisores(ll n) {
+        vl v;
+        for (ll i = 1; i <= n/2; i++) {
+                if (n % i == 0) v.pb(i);
+        }
+        return v;
+}
+
+
+void solve() {
+        cin>>n;
+        
+        if (x == 1) {
+                cout << x << " nao eh perfeito" << el;
+                return;
+        }
+
+        vl list =  divisores(n);
+
+        ll sum = soma(list);
+        
+        if (n == sum) cout << n << " eh perfeito" << el;
+        else cout << n << " nao eh perfeito" << el;
+
+
+}
+signed main() {
+        ___
+        t=1;
+        cin>>t;
+        while(t--)
+        solve();
+        
+        return 0;
+}
