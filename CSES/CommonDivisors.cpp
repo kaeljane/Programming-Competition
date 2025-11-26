2c35c42e5b47f8f2cee2e0d082faf12290f0ccbf
Kaeljane <kayyt0102@gmail.com>
2025-11-26 12:43:07 -0300

    Add Common Divisors | CSES

---
+++ CSES/CommonDivisors.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 26/11/2025 by Kaeljane
+using namespace std;
+ 
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+
+
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+ 
+/* (mind) 
+        
+        
+*/
+
+const int MAXN = 1e6;
+ll qt[MAXN + 1] = {0}; // array de frequencias
+
+void solve() {
+        cin>>n;
+        // jogar no arr global
+        for (ll i=0; i < n; i++) {
+                cin>>x;
+                qt[x]++;
+        }
+        
+        for (ll gcd = MAXN; gcd >= 1; gcd--) {
+                ll multiplos = 0;
+                for (ll j = gcd; j <= MAXN; j += gcd) {
+                        multiplos += qt[j];
+                }
+
+                if (multiplos >= 2) {
+                        cout << gcd << el;
+                        return;
+                }
+        }
+}
+
+signed main() {
+        ___
+        t=1;
+        //cin>>t;
+        //while(t--)
+        solve();
+        
+        return 0;
+}
