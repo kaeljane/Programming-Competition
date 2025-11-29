27c8964c567e8d5ef586e45c4ac49cf8bd04f244
Kaeljane <kayyt0102@gmail.com>
2025-11-29 19:16:57 -0300

    Add A_Mahmoud_and_Ehab_and_the_even_odd_game | CODEFORCE

---
+++ CodeForces/0800/A_Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 29/11/2025 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e6 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+    cin>>n;
+    // 0 -> Mahmoud | 1 -> Ehab
+    bool pVez = 0;
+
+    while (n!=0) {
+        if (pVez == 0) {
+            if (n % 2 == 0) break;
+            else n -= n-1;
+        }
+        else {
+            if (n % 2 != 0) break;
+            else n -= n-1;
+        }
+
+        if (pVez == 0) pVez = 1;
+        else pVez = 0;
+    }
+    if (pVez) cout << "Ehab" << el;
+    else cout << "Mahmoud" << el;
+    
+}
+signed main() {
+    ___
+    solve();
+    
+    return 0;
+}
