2fc3079cb54203d054a85ce99219bd124a1b882e
Kaeljane <kayyt0102@gmail.com>
2025-11-30 18:04:32 -0300

    Add B_Balanced_Array | CODEFORCES

---
+++ CodeForces/0800/B_Balanced_Array.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 30/11/2025 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e6 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    os iniciais n/2 sao pares 
+    o restante é impar
+    a soma dessas metades é igual
+    
+    pode ter multiplas respostas
+
+    n ->tamanho da lista
+        [sempre é par]
+
+*/
+void solve() {
+    cin>>n;
+    vl list(n);
+    if(n == 2) cout << "NO" << el;
+    else if ((n / 2) % 2 != 0) cout << "NO" << el;
+    else {
+        cout << "YES" << el;
+        // numeros pares
+        ll j=0;
+        ll somaPares=0;
+        for (ll i=2; j+1 <= n/2 ; i+=2) { 
+            list[j] = i;
+            somaPares+=list[j];
+            j++;
+            
+        }
+        // numeros impares
+        ll somaImpares=0;
+        for (ll i=1; j+1 <= n; i+=2) {
+            if (j == n-1) {
+                // ultimo numero
+                list[j] = somaPares - somaImpares;
+            }
+            else {
+                list[j] = i;
+                somaImpares += list[j];
+            }
+            j++;
+        }
+
+        for (auto x : list) cout << x << " ";
+        cout << el;
+    }
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
