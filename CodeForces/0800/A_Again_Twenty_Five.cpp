3ae7e74872b1955409a003e7d4e18f2d6dadc710
Kaeljane <kayyt0102@gmail.com>
2025-11-30 22:01:16 -0300

    Add A_Again_Twenty_Five | CODEFORCES

--- CodeForces/0800/A_Again_Twenty_Five.cpp
+++ CodeForces/0800/A_Again_Twenty_Five.cpp
@@ -1,27 +1,76 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define ld long double
+#define ii pair<ll, int>
 #define vi vector<int>
 #define vc vector<char>
+#define vb vector<bool>
 #define vs vector<string>
 #define vl vector<long long>
 #define vd vector<long double>
 #define vll vector<vector<long long>>
-#define all(x) x.begin(), x.end()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
 #define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
-// 22/10/2025 by Kaeljane
+// 30/11/2025 by Kaeljane
 using namespace std;
 
-void solve() {
-    ll n; cin>>n;
-    ll c = pow(5, n);
-    // qualquer numero elevado no 5 os dois ultimos numeros dele será 25, menos se elevar a a 0 ou 1.
-    cout << 25 << '\n';
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e6 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    formula
     
+*/
+
+ll binPow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) {
+            res = (res * a) % m;
+        }
+        a = (a*a) % m;
+        b >>= 1;
+    }
+    return res;
 }
-int main() {
-    ___
+
+void solve() {
+    cin>>n;
+    // versao hard nao precisa disso, colocando so par treinar
+    cout << binPow(5, n, 100) << el;
     
+
+    // a resposta sempre será 25
+    // cout << 25 << el;
+}
+signed main() {
+    ___
+    t=1;
+    //cin>>t;
+    //while(t--)
     solve();
     
     return 0;
