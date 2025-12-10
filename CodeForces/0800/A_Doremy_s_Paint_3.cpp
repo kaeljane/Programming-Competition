43939f6b4c4c997bbceeabb2323ec9a3b3822325
Kaeljane <kayyt0102@gmail.com>
2025-12-10 20:41:59 -0300

    Add A_Doremy_s_Paint_3 | CODEFORCES

---
+++ CodeForces/0800/A_Doremy_s_Paint_3.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 09/12/2025 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e6 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    1-n indexação
+    
+*/
+void solve() {
+    cin>>n;
+    // vl v(n);
+    map<ll, int> mapa;
+    // if (n == 2) {
+    //     cout << "YES" << el;
+    //     return;
+    // }
+    for (ll i=0; i<n;i++) {
+        cin>>x;
+        mapa[x]++;
+    }
+    vl qt; // 2, 1
+
+    if (mapa.size() == 1) {cout << "YES" << el; return;}
+
+    if (mapa.size() == 2) {
+        for (auto x : mapa) {
+            qt.push_back(x.second);
+        }
+    }
+    else {cout << "NO" << el; return;}
+
+    // pensando em quando tamanho do mapa == 2 
+    if (n % 2 == 0 && qt[0] == qt[1]) cout << "YES" <<el;
+    else if (n % 2 != 0 && (qt[0] == qt[1] - 1 || qt[0] == qt[1] + 1)) cout << "YES" << el;
+    else cout << "NO" << el;
+    
+
+
+    
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
