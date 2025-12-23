fee56ff7acb9e4248df64ff680799ef107941d62
Kaeljane <kayyt0102@gmail.com>
2025-12-23 18:26:06 -0300

    Add B_Square_Difference | CODEFORCES

---
+++ CodeForces/1100/B_Square_DifferenceFirst1100.cpp
@@ -0,0 +1,86 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 23/12/2025 by Kaeljane
+using namespace std;
+ 
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e6 + 5;
+// matriz vll mat(n, vl(m, 0))
+ 
+ll n, t, k, x, y, z, a, b;
+string s, c;
+ 
+/* (mind) 
+    diferenca de quadrados
+    a^2 - b^2 = (a-b) * (a+b)
+    
+*/
+bool isPrimo(ll n) {
+    if(n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0) return false;
+ 
+    for (ll i=3; i*i <= n; i+=2) {
+        if (n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+void solve() {
+    cin>>a>>b;
+ 
+    if ((a-b) == 1) {
+        if (isPrimo(a+b)) {
+            cout << "YES" << el;
+        }
+        else cout << "NO" << el;
+    }
+    else if ((a+b) == 1) {
+        if (isPrimo(a-b)) {
+            cout << "YES" << el;
+        }
+        else cout << "NO" << el;
+    }
+    else cout << "NO" << el;
+    
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
