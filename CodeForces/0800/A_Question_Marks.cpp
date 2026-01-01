2efeb6f2e57ebf29baf1167dd5a6774b406e4b27
Kaeljane <kayyt0102@gmail.com>
2025-12-31 23:11:46 -0300

    Add A_Question_Marks | CODEFORCES

---
+++ CodeForces/0800/A_Question_Marks.cpp
@@ -0,0 +1,82 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define ii pair<ll, int>
+#define vi vector<int>
+#define vc vector<char>
+#define vb vector<bool>
+#define vs vector<string>
+#define vl vector<long long>
+#define vd vector<long double>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rev(v) reverse(all(v))
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define sz(v) ((int)(v).size())
+#define vazio(v) (v).empty()
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define umap unordered_map
+#define uset unordered_set
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 31/12/2025 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e6 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    23:04 do dia 31/12/2025
+    
+*/
+void solve() {
+    cin>>n;
+    cin>>s;
+    ll qtA=0;
+    ll qtB=0;
+    ll qtC=0;
+    ll qtD=0;
+    ll qtTotal=0;
+
+    for (auto c : s) {
+        if (c == 'A') qtA++;
+        if (c == 'B') qtB++;
+        if (c == 'C') qtC++;
+        if (c == 'D') qtD++;
+    }
+
+    if (qtA > n) qtTotal += n;
+    else qtTotal += qtA;
+    if (qtB > n) qtTotal += n;
+    else qtTotal += qtB;
+    if (qtC > n) qtTotal += n;
+    else qtTotal += qtC;
+    if (qtD > n) qtTotal += n;
+    else qtTotal += qtD;
+    
+    cout << qtTotal << el;
+
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
