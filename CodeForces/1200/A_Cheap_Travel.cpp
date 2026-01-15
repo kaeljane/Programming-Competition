adbb4f38bf282c96dc3a4d06e32e7701a474ee43
Kaeljane <kayyt0102@gmail.com>
2026-01-15 17:44:36 -0300

    Add A_Cheap_Travel | CODEFORCES

---
+++ CodeForces/1200/A_Cheap_Travel.cpp
@@ -0,0 +1,61 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 15/01/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, a, b, m;
+string s, c;
+
+/* (mind) 
+    
+
+*/
+void solve() {
+    cin>>n>>m>>a>>b;
+    
+    ll ans = INF;
+
+    for (ll x = 0; x <= n; x++) {
+        for (ll y = 0; y <= n; y++) {
+            if (x + y*m >= n) {
+                ans = min(ans, a*x + b*y);
+            }
+        }
+    }
+
+    cout << ans << el;
+    
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
