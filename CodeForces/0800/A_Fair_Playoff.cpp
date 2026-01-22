c7bbfda373bb0300517a28d9709477d2d8521747
Kaeljane <kayyt0102@gmail.com>
2026-01-22 11:34:50 -0300

    Add A_Fair_Playoff | CODEFORCES

---
+++ CodeForces/0800/A_Fair_Playoff.cpp
@@ -0,0 +1,62 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 22/01/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+
+    vl v(4); 
+    for (ll i = 0; i < 4; i++) cin>>v[i];
+
+    ll f1 = max(v[0], v[1]);
+    ll f2 = max(v[2], v[3]);
+
+    sort(all(v));
+    if ((f1 == v[2] || f1 == v[3]) && (f2 == v[2] || f2 == v[3])) {
+        cout << "YES" << el;
+    }
+    else {
+        cout << "NO" << el;
+    }
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
