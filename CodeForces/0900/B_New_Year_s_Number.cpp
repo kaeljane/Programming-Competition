61d07fe825043b7a09b032be4b6ab34aadf2b917
Kaeljane <kayyt0102@gmail.com>
2026-01-23 14:19:26 -0300

    Add B_New_Year_s_Number | CODEFORCES

---
+++ CodeForces/0900/B_New_Year_s_Number.cpp
@@ -0,0 +1,61 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 23/01/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    n = 2020*x + 2021*y
+    n = 2020*x + (2020 + 1)*y
+    n = 2020*x + 2020*y + y
+    
+    n = 2020(x+y) + y
+    
+    D = d*q + r
+    
+*/
+void solve() {
+    cin>>n;
+    if (n % 2020 <= n / 2020) {
+        cout << "YES" << el;
+    }
+    else {
+        cout << "NO" << el;
+    }
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
