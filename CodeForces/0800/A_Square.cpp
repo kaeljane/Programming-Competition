4d217f38d64a463f33bc5e2e9fc26dad65b9a5d2
Kaeljane <kayyt0102@gmail.com>
2026-01-26 11:58:03 -0300

    Add A_Square | CODEFORCES

--- CodeForces/0800/A_Square.cpp
+++ CodeForces/0800/A_Square.cpp
@@ -1,44 +1,63 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
 #include <bits/stdc++.h>
 #define ll long long
-#define ld long double
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
+#define ii pair<ll, int>
 #define vl vector<long long>
-#define vd vector<long double>
 #define vll vector<vector<long long>>
-#define all(v) v.begin(), v.end()
-#define imp(v, t) copy(v.begin(), v.end(), ostream_iterator<t> (cout, ' '))
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
 #define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
-// 28/10/2025 by Kaeljane
+// 26/01/2026 by Kaeljane
 using namespace std;
 
-ll n, t, k, x, y, z;
-string s, a, b, c;
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, a, b;
+string s, c;
 
 /* (mind) 
     
     
 */
 void solve() {
-    
-    cin>>t;
-    while (t--) {
-        set<int> v = {};
-        
-        for(ll i=0; i<4; i++){
-            cin>>n; 
-            v.insert(n);
-        }
+    ll maiorX = -INF;
+    ll menorX = +INF;
 
-        if (v.size() == 1) cout<< "YES" << '\n';
-        else cout << "NO" << '\n';
+    ll maiorY = -INF;
+    ll menorY = +INF;
+
+    for (ll i = 0; i < 4; i++) {
+        cin>>a>>b;
+        maiorX = max(maiorX, a);
+        menorX = min(menorX, a);
+
+        maiorY = max(maiorY, b);
+        menorY = min(menorY, b);
     }
+
+    cout << (maiorX - menorX) * (maiorY - menorY) << el;
     
 }
-int main() {
+signed main() {
     ___
-    
+    cin>>t;
+    while(t--)
     solve();
     
     return 0;
