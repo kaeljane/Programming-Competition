bce6914ff5953232148d0c9d20d332c45a5a1702
Kaeljane <kayyt0102@gmail.com>
2026-02-09 12:00:01 -0300

    Add A_Pretty_Permutations | CODEFORCES

---
+++ CodeForces/0800/A_Pretty_Permutations.cpp
@@ -0,0 +1,62 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 08/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+    cin>>n;
+    if (n%2==0) {
+        for (ll i = 1; i < n; i+=2) {
+            cout << i+1 << " " << i << " ";
+        }
+    }
+    else {
+        for (ll i = 1; i < n-2; i+=2) {
+            cout << i+1 << " " << i << " ";
+        }
+        if (n >= 3) cout << n - 1 << " " << n << " "<< n - 2;
+        
+    }
+    cout << el;
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
