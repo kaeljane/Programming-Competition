888406825ccff7338d03e8dc481aed454e82606e
Kaeljane <kayyt0102@gmail.com>
2026-02-10 13:30:36 -0300

    Add B_Alphabetical_Strings | CODEFORCES

---
+++ CodeForces/0800/B_Alphabetical_Strings.cpp
@@ -0,0 +1,83 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 10/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    two pointer
+    foi difícil visualizar
+    
+*/
+void solve() {
+    cin>>s;
+    ll n = s.size();
+
+    ll posA = -1;
+    for (ll i = 0; i < n; i++) {
+        if (s[i] == 'a') {
+            posA = i;
+            break;
+        }
+    }
+
+    if (posA == -1) {
+        cout << "NO" << el;
+        return;
+    }
+
+    ll l = posA - 1;
+    ll r = posA + 1;
+
+    for (ll c = 'b'; c < ('a' + n); c++) {
+        if (l >= 0 && s[l] == c) {
+            l--;
+        }
+        else if (r < n && s[r] == c) {
+            r++;
+        }
+        else {
+            cout << "NO" << el;
+            return;
+        }
+    }
+
+    cout << "YES" << el;
+
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
