068b42e51d6e43efe44df064684b8bf1875e0562
Kaeljane <kayyt0102@gmail.com>
2026-02-11 00:22:26 -0300

    Add B_Go_Straight_and_Turn_Right | CODEFORCES

---
+++ Atcoder/B_Go_Straight_and_Turn_Right.cpp
@@ -0,0 +1,85 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 10/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+    cin>>n;
+    cin>>s;
+    // 1 leste, 2 sul, 3 oeste, 4 norte
+
+    ll indoPara = 1;
+
+    pair<ll, ll> par = {0, 0};
+    for (ll i = 0; i < n; i++) {
+        if (indoPara % 4 == 0) {
+            indoPara = 4;
+        }
+        else {
+            indoPara = indoPara % 4;
+        }
+        if (s[i] == 'S') {
+            if (indoPara == 1) {
+                par.first++;
+            }
+            else if (indoPara == 2) {
+                par.second--;
+            }
+            else if (indoPara == 3) {
+                par.first--;
+            }
+            else if (indoPara == 4) {
+                par.second++;
+            }
+        }
+        else {
+            indoPara++;
+
+        }
+
+    }
+
+    cout << par.first << " " << par.second << el;
+
+    
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
