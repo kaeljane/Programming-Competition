e21c5efc2e30b56f6abbc0bc79ad2453ed2e3858
Kaeljane <kayyt0102@gmail.com>
2026-02-14 19:09:18 -0300

    Update revision B_Same_Map_in_the_RPG_Worl

---
+++ revision/B_Same_Map_in_the_RPG_World.cpp
@@ -0,0 +1,77 @@
+// ------------ Discipline is remembering who you said you wanted to be^^ ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 14/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, m, h, w;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+    cin>>h>>w;
+    vector<string> A(h);
+    vector<string> B(h);
+    
+    for (ll i = 0; i < h; i++) cin>>A[i];
+    for (ll i = 0; i < h; i++) cin>>B[i];
+
+    for (ll s = 0; s < h; s++) {
+        for (ll t = 0; t < w; t++) {
+
+            bool equal = true;
+            for (ll i = 0; i < h; i++) {
+                for (ll j = 0; j < w; j++) {
+                    if (A[(s+i) % h][(t + j) % w] != B[i][j]) {
+                        equal = false;
+                        i = h;
+                        break;
+                    }
+                }
+            }
+            if (equal) {
+                cout << "Yes" << el;
+                return;
+            }
+        }
+    }
+    cout << "No" << el;
+
+    
+    
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
