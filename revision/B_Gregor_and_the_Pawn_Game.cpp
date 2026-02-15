3dd0dc0a386144781331b63f6ad9cb27cf117842
Kaeljane <kayyt0102@gmail.com>
2026-02-15 16:19:45 -0300

    Update revision B_Gregor_and_the_Pawn_Game

---
+++ revision/B_Gregor_and_the_Pawn_Game.cpp
@@ -0,0 +1,87 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 15/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    revision
+    quantos pioes pode ir para a row 1
+    
+*/
+void solve() {
+    cin>>n;
+    string s1; cin>>s1;
+    string s2; cin>>s2;
+    
+    ll qt = 0;
+
+    for (ll i = 0; i < n; i++) {
+        if (s2[i] == '1') {
+            if (i == 0) {
+                if (s1[i] == '0') {
+                    qt++;
+                    s1[i] = '2';
+                }
+                else if ( (i < n-1) && s1[i+1] == '1' ) {
+                    qt++;
+                    s1[i+1] = '2';
+                }
+            }
+            else {
+                if (s1[i-1] == '1') {
+                    qt++;
+                    s1[i-1] = '2';
+                }
+                else if (s1[i] == '0') {
+                    qt++;
+                    s1[i] = '2';
+                }
+                else if ( (i < n-1) && s1[i+1] == '1' ) {
+                    qt++;
+                    s1[i+1] = '2';
+                }
+            }
+        }
+    }
+
+    cout << qt << el;
+    
+
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
