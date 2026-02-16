33b6e53e94cf12466bc5f8a41b3f90308fc29239
Kaeljane <kayyt0102@gmail.com>
2026-02-15 22:21:53 -0300

    Addition A_Business_trip | CODEFORCES

---
+++ CodeForces/0900/A_Business_trip.cpp
@@ -0,0 +1,69 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 15/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+     < 1000 cm - os pais nao vai acreditar que ele regou
+    
+*/
+void solve() {
+    cin>>n;
+    vl v(12); for (ll i = 0; i < 12; i++) cin>>v[i];
+    sort(all(v));
+    
+    if (n == 0) {
+        cout << 0 << el;
+        return;
+    }
+    ll ac = 0;
+    ll qt = 0;
+    for (ll i = 11; i >= 0; i--) {
+        if (ac < n) {
+            ac += v[i];
+            qt++;
+        }
+        else {
+            break;
+        }
+    }
+    if (ac < n) cout << -1 << el;
+    else cout << qt << el;
+
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
