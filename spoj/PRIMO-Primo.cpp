e00f996afeae705f5d1b1f7feef55a9583348478
Kaeljane <kayyt0102@gmail.com>
2026-02-26 09:30:49 -0300

    Addition PRIMO-Primo | SPOJ

---
+++ spoj/PRIMO-Primo.cpp
@@ -0,0 +1,70 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 26/02/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+
+vector<bool> is_prime(MAXN, true);
+
+void sieve(ll n) {
+    is_prime[0] = is_prime[1] = false;
+
+    for (ll i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (ll j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+
+}
+
+
+void solve() {
+    cin>>n;
+    if (is_prime[n]) cout << "SIM" << el;
+    else cout << "NO" << el; 
+    
+}
+signed main() {
+    ___
+    sieve(MAXN);
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
