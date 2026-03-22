fb7ade1264d57b14c6e85383850c4891fe146bfb
Kaeljane <kayyt0102@gmail.com>
2026-03-22 12:28:17 -0300

    Add E - Seletiva POP 03/2026

---
+++ contests-pop/seletiva-pop-2026-03/e.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define el '\n'
+#define vl vector<long long>
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+
+using namespace std;
+// Seletiva POP
+// Bora se divertir
+
+ll INF = 1e18 + 7;
+
+ll n, t, l, x, y, z;
+string s, a, b, c;
+
+/* (mind )
+    
+
+*/
+
+void solve(){
+    cin>>n;
+    if (n >= 2014 && n <= 2026) {
+        cout << "YES" << el;
+    }
+    else {
+        cout << "NO" << el;
+    }
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--) 
+    solve();
+
+    return 0;
+}
