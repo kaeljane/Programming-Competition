0e02ae35fa10af7b5598ddb795de71afa8a7ed08
Kaeljane <kayyt0102@gmail.com>
2026-03-22 15:28:11 -0300

    A_Reserve_a_Substring | CODEFORCES

---
+++ CodeForces/1000/A_Reverse_a_Substring.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define el '\n'
+#define vl vector<long long>
+#define all(v) (v).begin(), (v).end()
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+using namespace std;
+// 22/03/2026
+// I can it
+
+ll INF = 1e18 + 7;
+
+ll n, t, l, x, y, z;
+string s, a, b, c;
+
+/* (mind )
+    
+*/
+
+void solve(){
+    cin>>n;
+    cin>>s;
+
+    for (ll l = 0; l < n-1; l++) {
+        if (s[l] > s[l+1]) {
+            cout << "YES" << el << l + 1 << " " << l + 2 << el;
+            return;
+        }
+    }
+    cout << "NO" << el;
+    
+    
+    
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--) 
+    solve();
+
+    return 0;
+}
