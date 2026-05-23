c084aa03167af99fb76559aeeeddc429f1158fed
Kaeljane <kayyt0102@gmail.com>
2026-05-23 20:44:03 -0300

    Add G | MFP 2026

---
+++ MFP/prova/g.cpp
@@ -0,0 +1,62 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 23/05/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    tentar deixar um numero impar para o prof 
+    
+    se for potencia de dois ele ganha!!!
+    
+    se for par sem ser potencia de dois vc perde
+
+
+
+*/
+void solve() {
+    cin>>n;
+    // vl v(n); for (ll i = 0; i < n; i++) cin>>v[i];
+    
+    // vector<ll> dois = {1};
+    if (n % 3 == 0) cout << "PERDE" << el;
+    else cout << "GANHA" << el;
+
+
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
