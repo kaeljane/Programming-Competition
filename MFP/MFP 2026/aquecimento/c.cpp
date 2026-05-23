98db7f0a82eae5fd960f2faefe0e5aa2688406a6
Kaeljane <kayyt0102@gmail.com>
2026-05-23 00:02:59 -0300

    Add C | Aquecimento MFP 2026

---
+++ MFP/MFP 2026/aquecimento/c.cpp
@@ -0,0 +1,72 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 16/05/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    
+    
+*/
+void solve() {
+    cin>>s;
+    
+    bool letra = false;
+    bool num = false;
+
+    string sBack = s;
+    sort(all(sBack));
+    n = s.size();
+
+    if (s.size() > 10) {
+        if (s == sBack) {
+            f (i, 0, n) {
+                if (s[i] >= '0' && s[i] <= '9') num = true;
+                if (s[i] >= 'a' && s[i] <= 'z') letra = true; 
+            }
+            if (letra && num) {
+                cout << "S" << el;
+                return;
+            }
+        }
+    }
+    
+    cout << "N" << el;
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
