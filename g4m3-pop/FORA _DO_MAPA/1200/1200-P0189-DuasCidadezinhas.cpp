f88b29b8efb303e00ede9ef14c21a9807e3b41e1
Kaeljane <kayyt0102@gmail.com>
2026-05-29 23:32:11 -0300

    Add P0189 | g4m3

---
+++ g4m3-pop/FORA _DO_MAPA/1200/1200-P0189-DuasCidadezinhas.cpp
@@ -0,0 +1,64 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 29/05/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, total;
+string s, a, b, c;
+
+/* (mind) 
+    prestar atencao no texto!!!
+    
+*/
+void solve() {
+    cin>>n>>total;
+    
+    ll soma = 0;
+    ll ansMinimo = INF;
+
+    f (i, 0, n) {
+        cin>>x;
+        soma += x;
+        ansMinimo = min(ansMinimo, abs(soma - (total - soma)));
+    }
+
+    if (n == 1) ansMinimo = total;
+    
+    cout << ansMinimo << el;
+
+    
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
