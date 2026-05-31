3cd1903c344098c46cb0a9ea4ffee9f0cdb0121f
Kaeljane <kayyt0102@gmail.com>
2026-05-30 21:32:29 -0300

    Add P0135 | g4m3

---
+++ g4m3-pop/FORA _DO_MAPA/1300/1300-P0135-Dharlan-o-Prefeito-Casamenteiro.cpp
@@ -0,0 +1,65 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 30/05/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    53min 27sec
+    
+*/
+ll lcm(ll a, ll b){
+    return a / __gcd(a, b) * b;
+}
+
+set<ll> ans;
+void solve() {
+    cin>>n;
+    ll bairro = 1;
+    f (i, 0, n) {
+        cin>>x;
+        bairro = lcm(bairro, x);
+    }
+    
+    ans.insert(bairro);
+    
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+
+    for (auto &k : ans) cout << k << el;
+    
+    return 0;
+}
