2dcdc88687efbd7e18b871bbb73056e1ba3e7ca2
Kaeljane <kayyt0102@gmail.com>
2026-06-13 23:09:09 -0300

    Add Removing Digits | CSES

---
+++ CSES/03.Dynamic_Programming/05.Removing_Digits.cpp
@@ -0,0 +1,71 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 13/06/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+string s, a, b, c;
+
+/* (mind) 
+    Parecida com Minimizing Coins
+    
+*/
+void solve() {
+    cin>>n;
+    
+    vector<ll> dp(n+1, INF); // minimo
+
+    dp[0] = 0;
+    f (i,0,n+1) {
+        ll numero = i;
+
+        while (numero > 0) {
+            ll digitoFinal = numero % 10;
+
+            if (digitoFinal != 0) {
+                dp[i] = min(dp[i], dp[i - digitoFinal] + 1);
+            }
+
+            numero /= 10;
+
+        }
+
+    }
+
+
+    cout << dp[n] << el;
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
