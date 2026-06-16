9dc8275b715655443d89135079bb3edce26ae454
Kaeljane <kayyt0102@gmail.com>
2026-06-15 23:13:20 -0300

    Add Ilya and Queries | CODEFORCES

---
+++ CodeForces/1100/B_Ilya_and_Queries.cpp
@@ -0,0 +1,70 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 15/06/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, m;
+string s, a, b, c;
+
+/* (mind) 
+    
+*/
+void solve() {
+    cin>>s>>m;
+    n = s.size();
+    vector<ll> dp(n+1, 0);
+
+    char cp = s[0];
+    s = '.' + s;
+    f (i,2,n+1) {
+        if (s[i-1] == s[i]) {
+            dp[i] = dp[i-1] + 1;
+        }
+        else {
+            dp[i] = dp[i-1];
+        }
+    }
+    
+    f (i, 0, m) {
+        cin>>x>>y;
+
+        cout << dp[y] - dp[x] << el;
+
+    }
+    
+    
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
