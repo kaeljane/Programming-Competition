4ec4f6c0750c30aa5aa2127cd030df6bf09dc989
Kaeljane <kayyt0102@gmail.com>
2026-07-21 19:05:52 -0300

    Add P0095 | g4m3 - PICA PAU

---
+++ g4m3-pop/PICA_PAU/P0095.cpp
@@ -0,0 +1,71 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, int>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 21/07/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z;
+bool ans=0;
+string s, a, b, c;
+
+/* (mind) 
+    Time: (9min 9sec) esq de coloca -> 11min 14sec
+    Padrão: geometria basica
+
+    720cm
+    |
+    |
+    |
+    |
+    |
+    ----------------0----------------
+
+    (<-22->)
+    
+    questao 90
+
+*/
+void solve() {
+    cin>>n;
+    f (i, 0, n) {
+        cin>>x>>y;
+        if (y >= 0 && y <= 720 &&  (x - 11) > 6) {
+            ans=1;
+        }
+    }
+    if (ans) cout << "GOL" << el;
+    else cout << "LANCE QUE SEGUE" << el;
+}
+signed main() {
+    ___
+    // cin>>t;
+    // while(t--)
+    solve();
+    
+    return 0;
+}
