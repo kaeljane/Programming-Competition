255106783a1e338e5be46f063db7826ac03a35bd
Kaeljane <kayyt0102@gmail.com>
2026-07-22 17:02:46 -0300

    Add P0204 | g4m3 - PICA PAU

---
+++ g4m3-pop/PICA_PAU/P0204.cpp
@@ -0,0 +1,64 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 22/07/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans;
+string s, a, b, c;
+
+/* (mind) 
+    Time: 4min 3sec
+    Padrão: array, sorting
+
+    duplicar altura do personagem
+    
+*/
+void solve() {
+    cin>>n;
+    vl v(n); f (i, 0, n) cin>>v[i];
+    sort(rall(v));
+    ans = 0;
+    f (i, 1, n) {
+        while (v[i] < v[i-1]) {
+            v[i] *= 2;
+            ans++;
+        }
+    }
+
+    cout << ans << el;
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
