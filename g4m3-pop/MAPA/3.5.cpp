181440e1551fc28f043acd2d406fd1a0fdee9687
Kaeljane <kayyt0102@gmail.com>
2026-07-22 22:47:18 -0300

    Add 3.5 | g4m3

---
+++ g4m3-pop/MAPA/3.5.cpp
@@ -0,0 +1,68 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 22/07/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans, a;
+string s, b, c;
+
+/* (mind) 
+    Time: 8min 29sec
+    Padrão: array
+
+    quando invande vc esconde a semente mais valiosa
+    quando nao aparece ninguem vc estoca uma semente
+    
+*/
+void solve() {
+    cin>>n;
+    vl v;
+    while (cin>>a) {
+        if (a != 0) {
+            v.pb(a);
+            if (v.size() == n) break;
+            sort(all(v));
+
+        }
+        else {
+            cout << v.back() << el;
+            v.pop_back();
+        }
+    }
+    
+    
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
