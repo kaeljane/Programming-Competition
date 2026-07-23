23f25d14ebe48a4c549ea47ef8a648b374180200
Kaeljane <kayyt0102@gmail.com>
2026-07-22 21:21:41 -0300

    Add 2.5 | g4m3

---
+++ g4m3-pop/MAPA/2-5.cpp
@@ -0,0 +1,94 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 22/07/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans;
+string s, a, b, c;
+
+/* (mind) 
+    Time: 40min 14sec
+    Padrão: string
+
+    O - nao esta olhando para o esconderijo
+    X - olhando para o esconderijo
+
+    
+*/
+void solve() {
+    ll pInicial = 0;
+    
+    while (cin>>s) {
+
+        bool b1=0;
+
+        for (auto c : s) {
+            if (c == 'X') {b1=1; break;}
+        }
+
+        if (!b1) break;
+
+        if (s[pInicial] == 'X' ) {
+            cout << "Silêncio..." << el;
+        }
+        else {
+
+            bool podeEsq = (pInicial > 0 && s[pInicial - 1] == 'O');
+            bool podeDir = (pInicial < s.size()-1 && s[pInicial + 1] == 'O');
+
+            if (podeEsq && !podeDir) {
+                pInicial--;
+                cout << "Correndo pro esconderijo " << pInicial << "!" << el;
+            }
+            else if (!podeEsq && podeDir) {
+                pInicial++;
+                cout << "Correndo pro esconderijo " << pInicial << "!" << el;
+            }
+            else {
+                cout << "Tiro de Melão!!!" << el;
+                ans++;
+            }
+
+        }
+    }
+
+    cout << "Vitória com " << ans << " melões!" << el;
+
+    
+    
+    
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
