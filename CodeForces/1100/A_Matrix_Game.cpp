a3e71ea08e337c27a6cb9eb00b9328687d2a5233
Kaeljane <kayyt0102@gmail.com>
2026-07-27 16:05:59 -0300

    Add A_Matrix_Game | CODEFORCES

---
+++ CodeForces/1100/A_Matrix_Game.cpp
@@ -0,0 +1,85 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 27/07/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans, m;
+string s, a, b, c;
+
+/* (mind) 
+    Time: 48min 44sec -> 55min
+    Padrão: matriz, propagacao
+
+    treinando padrao
+    
+    ingles dessa questao queimou meu cerebro
+
+    Diagonal seria o ideal? acho que sim
+
+*/
+
+void solve() {
+    cin>>n>>m;
+
+    vll start(n, vl(m, 0));
+
+    f (i,0,n) {
+        f (j,0,m) {
+            cin>>start[i][j];
+        }
+    }
+    bool jogador = 0; // 0 -> A, 1 -> V
+    f (i,0,n) {
+        f (j,0,m) {
+            bool b1=1;
+
+            f (k, 0, m) if (start[i][k]) {b1=0; break; f (i,0,m) start[i][k]=1;}
+            f (k, 0, n) if (start[k][j]) {b1=0; break; f (i,0,n) start[k][j]=1;}
+ 
+            if (b1) {
+                start[i][j] = 1;
+                jogador = !jogador;
+            }
+        }
+    }
+
+    if (jogador&1) cout << "Ashish" << el;
+    else cout << "Vivek" << el;
+
+    
+    
+}
+signed main() {
+    ___
+    cin>>t;
+    while(t--)
+    solve();
+    
+    return 0;
+}
