22d62efce3423d9542027b29862847c8e1e6aac4
Kaeljane <kayyt0102@gmail.com>
2026-07-31 13:07:33 -0300

    Add A_Dragons | CODEFORCES

--- CodeForces/1000/A_Dragons.cpp
+++ CodeForces/1000/A_Dragons.cpp
@@ -21,6 +21,8 @@
 #define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
 #define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
 // 13/01/2026 by Kaeljane
+// 31/07/2026 by Kaeljane
+
 using namespace std;
 
 ll MOD = 1e9 + 7;
@@ -29,50 +31,52 @@ const int MAXN = 1e6 + 5;
 // matriz vll mat(n, vl(m, 0))
 
 ll n, s, t, k, x, y, z;
-string  a, b, c;
+string a, b, c;
 
 /* (mind) 
-    derrotar n dragoes
-    forças
-    força de kirito -> s
-
-    out
-    s n
-    ...
-
-    ele recebe Yi de força de matar o dragao
-
-    s = 2
-
-    OBS:
-        EM QUALQUER ORDEM!!! PRESTA ATENÇÃO!!!  presta atencao pow
-        ERA UM SINAL DE IGUAL DAKSDASDA
+    
         
 */
+
+struct Forcas {
+    ll forca;
+    ll bonus;
+};
+
+bool cmp (Forcas a, Forcas b) {
+    if (a.forca != b.forca) {
+        return a.forca < b.forca;
+    }
+
+    return a.bonus > b.bonus;
+}
+
 void solve() {
     cin>>s>>n;
- 
-    vector<ii> pares;
-    for (ll i = 0; i < n; i++) {
-        cin>>x>>y;
-        pares.pb({x, y});
+
+    vector<Forcas> v(n);
+
+    f (i, 0, n) {
+        cin>>v[i].forca>>v[i].bonus;
     }
-    sort(all(pares));
- 
-    for (auto &x : pares) {
-        if (s > x.first) {
-            s += x.second;
+
+    sort(all(v), cmp);
+
+    f (i, 0, n) {
+        if (s > v[i].forca) {
+            s += v[i].bonus;
         }
         else {
             cout << "NO" << el;
             return;
         }
     }
- 
+
     cout << "YES" << el;
-    
-    
+
+
 }
+
 signed main() {
     ___
     // cin>>t;
