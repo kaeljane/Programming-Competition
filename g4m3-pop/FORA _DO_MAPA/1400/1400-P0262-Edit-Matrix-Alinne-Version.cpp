06d277f926760bc2e04cddf12d373c9e87f23ce7
Kaeljane <kayyt0102@gmail.com>
2026-08-02 20:18:34 -0300

    Add P0262 | g4m3 - Mulher Tech

---
+++ g4m3-pop/FORA _DO_MAPA/1400/1400-P0262-Edit-Matrix-Alinne-Version.cpp
@@ -0,0 +1,108 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 02/08/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans, l, c, s;
+string a, b;
+
+/* (mind) 
+    Time: 27min 25sec
+    Padrão: 
+
+    submatriz
+
+    pegadinha
+
+    rotacionou a submatriz em 90 graus para a esquerda
+    
+*/
+
+void rotacao90(vll &mat) {
+    f (i, 0, s) {
+        f (j, i+1, s) {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+
+    f (i, 0, s) {
+        reverse(mat[i].begin(), mat[i].end());
+    }
+}
+
+void solve() {
+    cin>>n>>l>>c>>s;
+    vll mat(n, vl(n, 0));
+    vll sub(s, vl(s, 0));
+    f (i, 0, n) {
+        f (j, 0, n) {
+            cin>>mat[i][j]; 
+            // cout << mat[i][j] << " ";
+        }
+        cout << el;
+    }
+    // cout << el;
+    f (i, 0, s) {
+        f (j, 0, s) {
+            sub[i][j] = mat[l+i][c+j];
+            // cout << sub[i][j] << " ";
+        }
+        // cout << el;
+    }
+
+    rotacao90(sub);
+
+    f (i, 0, s) {
+        f (j, 0, s) {
+            mat[l+i][c+j] = sub[i][j];
+            // cout
+            // sub[i][j] = mat[l+i][c+j];
+            // cout << mat[i][j] << " ";
+        }
+        // cout << el;
+    }
+
+    f (i, 0, n) {
+        f (j, 0, n) {
+            cout<<mat[i][j] << " "; 
+        }
+        cout << el;
+    }
+
+    
+    
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
