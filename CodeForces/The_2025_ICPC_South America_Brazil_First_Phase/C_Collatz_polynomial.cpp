829ceff0bf76f6a1f284a3d6935cef4beb8bc51f
Kaeljane <kayyt0102@gmail.com>
2026-08-05 23:43:07 -0300

    Add C_Collatz_Polynomial | SBC 2025 First Phase

---
+++ CodeForces/The_2025_ICPC_South America_Brazil_First_Phase/C_Collatz_polynomial.cpp
@@ -0,0 +1,93 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 05/08/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans;
+string s, a, b, c;
+
+/* (mind) 
+    Time: ///
+    Padrão: 
+
+    grau de polinomio
+
+    Ai = 1 -> o termo Xi está presente no polinomio
+    Ai = 0 -> o termo Xi não está presente no polinomio
+
+    
+*/
+void solve() {
+    cin>>n;
+    
+    vector<ll> v(n+1);
+    
+    rf (i, n, 0) {
+        cin>>v[i];
+    }
+
+    ll ops = 0;
+
+    while (v.size() > 1) {
+        ops++;
+        if (v[0] == 0) {
+            v.erase(v.begin());
+        }
+        else {
+            vector <ll> temp(v.size() + 1, 0);
+
+            f (i, 0, v.size()) {
+                temp[i+1] = v[i];
+            }
+
+            f (i, 0, v.size()) {
+                temp[i] = (temp[i] + v[i]) % 2;
+            }
+
+            temp[0] = (temp[0] + 1) % 2;
+
+            v = temp;
+
+            while (v.size() > 1 && v.back() == 0) {
+                v.pop_back();
+            }
+        }
+    }
+
+    cout << ops << el;
+
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
