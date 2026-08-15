0c588448e77c2bd0ecc635ee3d02dec236cd74be
Kaeljane <kayyt0102@gmail.com>
2026-08-15 19:58:27 -0300

    Add P0087 | g4m3 - PICA PAU

---
+++ g4m3-pop/PICA_PAU/P0087.cpp
@@ -0,0 +1,73 @@
+// ------------ Repetition repetition repetition repetition repetition repet... ------------
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<ll, ll>
+#define vl vector<long long>
+#define vll vector<vector<long long>>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define soma(v) accumulate(all(v), 0LL)
+#define max_el(v) *max_element(all(v))
+#define min_el(v) *min_element(all(v))
+#define f(i,b,e) for (ll i = (b); i < (e); i++)
+#define rf(i,b,e) for (ll i = (b); i >= (e); i--)
+#define fi first
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define sec second
+#define el '\n'
+#define imp(v, t, e) copy(v.begin(), v.end(), ostream_iterator<t> (cout, e))
+#define uset unordered_set
+#define ___ ios::sync_with_stdio(false); cin.tie(nullptr);
+// 15/08/2026 by Kaeljane
+using namespace std;
+
+ll MOD = 1e9 + 7; // 998244353;
+ll INF = 1e18 + 7;
+const int MAXN = 1e5 + 5;
+// matriz vll mat(n, vl(m, 0))
+
+ll n, t, k, x, y, z, ans;
+string s, a, b, c;
+
+/* (mind) 
+    Time: 18min 12sec
+    Padrão: fatorial, permutacao
+    
+    Summarize(resumir/descrever):
+        
+        
+    Deduce(deduzir/derivar/testar):
+        
+        
+    Solve(resolver/programar):
+    Change(TLE, WA):
+        
+*/
+void solve() {
+    cin>>n;
+    ans = 1;
+    map<ll, ll> mp;
+    vl v(n); f (i, 0, n) {
+        cin>>x>>y;
+        mp[y]++;
+    }
+
+    for(auto &x : mp) {
+        f (i, 1, x.sec+1) {
+            ans *= i;
+        }    
+    }
+    cout << ans << el;
+    
+}
+signed main() {
+    ___
+    //cin>>t;
+    //while(t--)
+    solve();
+    
+    return 0;
+}
