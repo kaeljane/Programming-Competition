a6d18b4285d09cc2bce1070f5d27f0d5a95cfae2
Kaeljane <113480990+kaeljane@users.noreply.github.com>
2026-09-10 11:19:02 -0300

    Add 1734 | neps

---
+++ neps/String Simples/1734.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define all(v) (v).begin(), (v).end()
+#define ll long long
+using namespace std;
+/*
+    
+*/
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t, n; char c;
+    cin>>t;
+    
+    while (t--) {
+        cin>>n>>c;
+        while (n--) {
+            cout << c;
+        }
+        cout << '\n';
+
+    }
+
+    return 0;
+}
