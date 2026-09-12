af56ee99fbf43391ab9091256836529eb75ffcf5
Kaeljane <kayyt0102@gmail.com>
2026-09-12 13:35:08 -0300

    Add 898 | neps

---
+++ neps/String Simples/898.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define all(v) (v).begin(), (v).end()
+#define ll long long
+using namespace std;
+/*
+
+*/
+
+void solve() {
+    string s; cin>>s;
+    cout << s[0] << s.size() - 2 << s[s.size()-1];
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // ll t; cin>>t; 
+    // while (t--)
+    solve();
+
+    return 0;
+}
